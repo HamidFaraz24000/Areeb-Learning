5661d15cbf1653d98cdc6b2bd65022ffd91f6404
Areeb Ibrahim <166850126+Areeb-Ibrahim@users.noreply.github.com>
2026-07-07 15:57:34 +0200

    init.py file
    Exports all database models

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from inventory_management.models.customer import Customer
+from inventory_management.models.order import Order, OrderItem
+from inventory_management.models.product import Product
+
+__all__ = ["Customer", "Order", "OrderItem", "Product"]
